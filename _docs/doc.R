c805e2a535a257160a3c308467c8c1e7f2fabe30
Jochen Kruppa <jkruppa@users.noreply.github.com>
2024-01-26 20:13:55 +0100

    Update doc.R

--- _docs/doc.R
+++ _docs/doc.R
@@ -6,7 +6,7 @@ cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73",
                 "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 ## ------------------------------------------------------------
 
-grade_tbl <- read_excel(file.path(path_home(), "Documents/GitHub/archive/_docs/grades.xlsx")) %>% 
+grade_tbl <- read_excel(file.path(path_home(), "work/GitHub/archive/_docs/grades.xlsx")) %>% 
   mutate(grade = as.character(grade))
 
 
@@ -27,15 +27,15 @@ ggplot(count_tbl, aes(x = grade, y = count, fill = grade)) +
   theme(legend.position = "none",
         axis.text.x = element_text(size=14),
         axis.text.y = element_text(size=14)) +
-  geom_text(data = count_tbl, aes(x = grade, y = count + 2, label = label),
-            size = 5) +
+  geom_label(data = count_tbl, aes(x = grade, y = count + 3, label = label),
+             size = 5, fill = "white") +
   geom_vline(xintercept = 10.5, linetype = 2) +
   annotate("label", x = 9.5, y = max(count_tbl$count), label = str_c("n = ", nrow(grade_tbl)),
            size = 8) +
   scale_y_continuous(breaks = seq(0, max(count_tbl$count) + 10, 10), 
                      limits = c(0,  max(count_tbl$count) + 10)) 
   
-ggsave(file.path(path_home(), "Documents/GitHub/archive/_docs/density.png"),
+ggsave(file.path(path_home(), "work/GitHub/archive/_docs/density.png"),
        width = 9, height = 6)
 
 
@@ -71,6 +71,6 @@ ggplot(count_year_tbl, aes(as.character(grade), n,
   geom_label(data = count_year_sum_tbl, aes(grade, n, label = sum_n),
              fill = "white")
 
-ggsave(file.path(path_home(), "Documents/GitHub/archive/_docs/density_year.png"),
-       width = 9, height = 9)
+ggsave(file.path(path_home(), "work/GitHub/archive/_docs/density_year.png"),
+       width = 9, height = (nrow(count_year_sum_tbl) * 3))
 
